9117e08f046cf880abf97fffdcc2cfaf81aa5fb5
zulfiyebiser <zlfdnbsr@gmail.com>
2019-12-05 11:33:46 -0600

    added Assgment

--- src/test/java/com/tests/vytrack/TestBase.java
+++ src/test/java/com/tests/vytrack/TestBase.java
@@ -1,12 +1,15 @@
 package com.tests.vytrack;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.utils.BrowserUtils;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import com.utils.ConfigurationReader;
 import com.utils.Driver;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 //this class will be a test foundation for all test classes
@@ -14,21 +17,37 @@ import java.util.concurrent.TimeUnit;
 //In this way before and after methods will be the same
 //Every test class will extend testbase class
 public abstract class TestBase {
+// * ExtentReports itself does not build any reports, but allows reporters to access information, which in
+// * turn build the said reports. An example of building an HTML report and adding information to ExtentX:
+// * ExtentHtmlReporter html = new ExtentHtmlReporter("Extent.html");
+// * ExtentXReporter extentx = new ExtentXReporter("localhost");
+
     protected ExtentReports extentReports;
-    //ExtentHtmlReporter creates a rich stanalone HTML file.It allows several
+    //    The ExtentHtmlReporter creates a rich standalone HTML file. It allows several
     protected ExtentHtmlReporter extentHtmlReporter;
-    // Defines  a test. You can add logs, snapshots,assign author and categories to a test and its children.
+    //    Defines a test. You can add logs, snapshots, assign author and categories to a test and its children.
+    protected ExtentTest extentTest;
 
     @BeforeTest
-    public void beforeTest() {
+    public void beforeTest(){
         //location of report
         //it's gonna be next to target folder, test-output folder
-        String filePath = System.getProperty("user.dir") + "test-output/report.html";
-
+        String filePath = System.getProperty("user.dir") + "/test-output/report.html";
         extentReports = new ExtentReports();
         extentHtmlReporter = new ExtentHtmlReporter(filePath);
         extentReports.attachReporter(extentHtmlReporter);
         extentHtmlReporter.config().setReportName("Vytrack Test Results");
+        //system information
+        extentReports.setSystemInfo("Environment", "QA1");
+        extentReports.setSystemInfo("Browser", ConfigurationReader.getProperty("browser"));
+        extentReports.setSystemInfo("OS", System.getProperty("os.name"));
+    }
+    //please comeback at 12:17
+
+    @AfterTest
+    public void afterTest(){
+//         Writes test information from the started reporters to their output view
+        extentReports.flush();
     }
 
 
@@ -36,34 +55,25 @@ public abstract class TestBase {
     public void setup(){
         String url = ConfigurationReader.getProperty("url");
         Driver.get().get(url);
-        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
     @AfterMethod
-    public void teardown(){
+    public void teardown(ITestResult result){
+
+        if(result.getStatus()==ITestResult.FAILURE){
+            extentTest.fail(result.getName());
+            extentTest.fail(result.getThrowable());
+            try {
+                // BrowserUtils.getScreenshot(result.getName())==>returns screenshot and returns location of that screenshot
+                extentTest.addScreenCaptureFromPath(BrowserUtils.getScreenshot(result.getName()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else  if (result.getStatus() == ITestResult.SKIP){
+            extentTest.skip("Test case was skipped: "+result.getName());
+        }
         Driver.close();
     }
 
 
-
-   // public static class LoginTest extends TestBase {
-//        @Ignore
-//        @Test
-//        public void test1(){
-//            //read url value from the properties file
-//            String url = ConfigurationReader.getProperty("url");
-//            //Driver.get() --> will return webdriver object
-//            //and then we can call webdriver methods like get(), findElement()....
-//    //        WebDriver driver = Driver.get();
-//            Driver.get().get(url);
-//            //print page title
-//            System.out.println(Driver.get().getTitle());
-//            BrowserUtils.wait(2);
-//            Driver.close();
-//        }
-//        @Test
-//        public void test2(){
-//            System.out.println(Driver.get().getTitle());
-//            BrowserUtils.wait(2);
-//        }
-    }
-
+}
