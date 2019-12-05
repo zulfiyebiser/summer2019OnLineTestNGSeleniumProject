22fb186c6d6675023f81f9182c1b2d1aa8157c18
zulfiyebiser <zlfdnbsr@gmail.com>
2019-12-05 09:25:13 -0600

    added Assgment

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import com.utils.BrowserUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriverException;
@@ -15,16 +16,22 @@ import com.utils.Driver;
 //top menu appears on every single page
 //so we can keep them here
 public class BasePage {
+
     @FindBy(css = "div[class='loader-mask shown']")
     public WebElement loaderMask;
+
     @FindBy(css = "h1[class='oro-subtitle']")
     public WebElement pageSubTitle;
+
     @FindBy(css = "#user-menu > a")
     public WebElement userName;
+
     @FindBy(linkText = "Logout")
     public WebElement logOutLink;
+
     @FindBy(linkText = "My User")
     public WebElement myUser;
+
     public BasePage() {
         //this method requires to provide webdriver object for @FindBy
         //and page class
@@ -32,55 +39,84 @@ public class BasePage {
         PageFactory.initElements(Driver.get(), this);
     }
 
-    /*
-    While this loading screen present, html code is a not complete
-    some elements will be missing
-    Also you won't be able to interact with any elements
-    All actions will be intercepted
-    Waits until loader mask(loading bar, spinning wheel)disappears
-    @return true of if loader mask is gone, false if something went wrong
+    /**
+     * While this loading screen present, html code is a not complete
+     * Some elements can be missing
+     * Also, you won't be able to interact with any elements
+     * All actions will be intercepted
+     * Waits until loader mask (loading bar, spinning wheel) disappears
+     *
+     * @return true if loader mask is gone, false if something went wrong
      */
-
-    public boolean waitUntilLoaderMaskDissapear(){
-        WebDriverWait wait = new WebDriverWait(Driver.get(),5);
-        try{
-            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
-            return  true;
-        }catch(NoSuchElementException e) {
+    public boolean waitUntilLoaderMaskDisappear() {
+        WebDriverWait wait = new WebDriverWait(Driver.get(), 5);
+        try {
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div[class='loader-mask shown']")));
+            return true;
+        } catch (NoSuchElementException e) {
             System.out.println("Loader mask not found!");
-            System.out.println(e.getMessage());
-            return  true; //no loader mask, all good, return true
-        }catch(WebDriverException e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return true; // no loader mask, all good, return true
+        } catch (WebDriverException e) {
+            e.printStackTrace();
         }
         return false;
     }
+
     /**
      * This method stands for navigation in vytrack app
      * provide tab name, for example "Fleet" as a String
      * and module name, for example "Vehicles" as a String as well
      * then based on these values, locators will be created
+     *
      * @param moduleName
-     * @param subModuleName
-     * normalize-space() same line .trim() in java
+     * @param subModuleName normalize-space() same line .trim() in java
      */
     public void navigateTo(String moduleName, String subModuleName) {
-
-        String moduleLocator = "//*[normalize-space()='"+moduleName+"' and @class='title title-level-1']";
-
-        String subModuleLocator = "//*[normalize-space()='"+subModuleName+"' and @class='title title-level-2']";
+        String moduleLocator = "//*[normalize-space()='" + moduleName + "' and @class='title title-level-1']";
+        String subModuleLocator = "//*[normalize-space()='" + subModuleName + "' and @class='title title-level-2']";
 
         WebDriverWait wait = new WebDriverWait(Driver.get(), 10);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(moduleLocator)));
+
         WebElement module = Driver.get().findElement(By.xpath(moduleLocator));
         wait.until(ExpectedConditions.visibilityOf(module));
         wait.until(ExpectedConditions.elementToBeClickable(module));
+
+        waitUntilLoaderMaskDisappear();
         module.click(); //once we clicked on module, submodule should be visible
+
         WebElement subModule = Driver.get().findElement(By.xpath(subModuleLocator));
         wait.until(ExpectedConditions.visibilityOf(subModule));
         subModule.click();
-        //after navigation
-        //wait until loader mask disappear
-        wait.until(ExpectedConditions.invisibilityOf(loaderMask));
     }
+
+    /**
+     * @return page name, for example: Dashboard
+     */
+    public String getPageSubTitle() {
+        //ant time we are verifying page name, or page subtitle, loader mask appears
+        waitUntilLoaderMaskDisappear();
+        BrowserUtils.waitForStaleElement(pageSubTitle);
+        return pageSubTitle.getText();
+    }
+
+    public String getUserName() {
+        waitUntilLoaderMaskDisappear();
+        BrowserUtils.waitForVisibility(userName, 5);
+        return userName.getText();
+    }
+
+    public void logOut() {
+        BrowserUtils.wait(2);
+        BrowserUtils.clickWithJS(userName);
+        BrowserUtils.clickWithJS(logOutLink);
+    }
+
+    public void goToMyUser() {
+        waitUntilLoaderMaskDisappear();
+        BrowserUtils.waitForClickablility(userName, 5).click();
+        BrowserUtils.waitForClickablility(myUser, 5).click();
+    }
+
 }
