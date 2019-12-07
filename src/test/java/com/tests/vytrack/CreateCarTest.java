a62334dc38bb9997c6b5e08c77a2ccb2b0afe098
zulfiyebiser <zlfdnbsr@gmail.com>
2019-12-07 08:01:55 -0600

    added Assgment

---
+++ src/test/java/com/tests/vytrack/CreateCarTest.java
@@ -0,0 +1,36 @@
+package com.tests.vytrack;
+import org.testng.annotations.Test;
+import pages.CreateCarPage;
+import pages.LoginPage;
+import pages.VehiclesPage;
+
+
+public class CreateCarTest extends TestBase {
+
+    @Test(description = "Create some random car")
+    public void test1(){
+        extentTest = extentReports.createTest("Create a new car");
+
+        LoginPage loginPage = new LoginPage();
+        VehiclesPage vehiclesPage = new VehiclesPage();
+        CreateCarPage createCarPage = new CreateCarPage();
+
+        loginPage.login("storemanager85", "UserUser123");
+        loginPage.navigateTo("Fleet", "Vehicles");
+
+        loginPage.waitUntilLoaderMaskDisappear();
+
+        vehiclesPage.clickToCreateAcar();
+
+        loginPage.waitUntilLoaderMaskDisappear();
+
+        createCarPage.licensePlateElement.sendKeys("Random");
+        createCarPage.selectTags("Compact");
+        createCarPage.selectFuelType("Diesel");
+
+        loginPage.waitUntilLoaderMaskDisappear();
+        createCarPage.saveAndCloseButtonElement.click();
+
+        extentTest.pass("New car was created");
+    }
+}
